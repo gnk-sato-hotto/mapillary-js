041bdf0ff9b114d4cf5285cd18cd5793cd512354
genkisato <satognk2014@gmail.com>
2025-05-15 13:49:30 +0900

    disable all step dom

--- src/component/direction/DirectionDOMRenderer.ts
+++ src/component/direction/DirectionDOMRenderer.ts
@@ -111,9 +111,11 @@ export class DirectionDOMRenderer {
         if (isSpherical(this._image.cameraType)) {
             // steps = steps.concat(this._createSphericalArrows(navigator, rotation));
         } else {
+            /*
             steps = steps.concat(
                 this._createPerspectiveToSphericalArrows(navigator, rotation));
-            // steps = steps.concat(this._createStepArrows(navigator, rotation));
+            steps = steps.concat(this._createStepArrows(navigator, rotation));
+            */
             turns = turns.concat(this._createTurnArrows(navigator));
         }
 
